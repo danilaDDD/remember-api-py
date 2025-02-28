f353692e1421699fb0451ca2e4eb64a66b3408e1
danila nikitin <danila.n2015@yandex.ru>
2025-02-28 13:06:45 +0300

    rename module rememberapi to remember-api

--- remember-api/asgi.py
+++ remember-api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for rememberapi project.
+ASGI config for remember-api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rememberapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'remember-api.settings')
 
 application = get_asgi_application()
