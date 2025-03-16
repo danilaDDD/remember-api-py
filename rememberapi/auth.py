ca2af0655a8d0730fcfcdf2b992f338e147e96a8
danila nikitin <danila.n2015@yandex.ru>
2025-03-16 20:27:04 +0300

    add simple jwt settings

--- rememberapi/auth.py
+++ rememberapi/auth.py
@@ -3,3 +3,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+SIMPLE_JWT = {
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': 'DJANGO_gdsfdsggdsgf23424344sdfgdsg',
+}
