9fc37ea489470d4f7fbac155c0c77f8ddc649d2f
danila nikitin <danila.n2015@yandex.ru>
2025-04-12 17:20:28 +0300

    fix timezone

--- common/middlewares.py
+++ common/middlewares.py
@@ -1,18 +1,18 @@
 import logging
 
+import pytz
 from django.http import HttpRequest
+from django.utils import timezone
 
 logger = logging.getLogger('request_logger')
 
-class LoggingNo20xMiddleware:
-    """
-    Middleware to log requests and responses, excluding 2xx responses.
-    """
+class CustomCommonMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request: HttpRequest):
+        timezone.activate(pytz.timezone('Europe/Moscow'))
         response = self.get_response(request)
 
         # Log the response if it's not a 2xx status code
--- rememberapi/settings.py
+++ rememberapi/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'common.middlewares.LoggingNo20xMiddleware',
+    'common.middlewares.CustomCommonMiddleware',
 ]
 
 ROOT_URLCONF = 'rememberapi.urls'
